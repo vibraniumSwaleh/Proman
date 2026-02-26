fb4b5d562ee1c7e2e4cc4e060563e22baf43cc94
Swaleh Hussein <hus.swaleh@yahoo.com>
2026-02-26 14:45:18 +0000

    start page layout

---
+++ app/dashboardWrapper.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
+      {/* Sidebar */}
+      sidebar
+      <main
+        className={`dark:bg-dark-bg flex w-full flex-col bg-gray-50 md:pl-64`}
+      >
+        navbar
+        {/* {navbar} */}
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default DashboardWrapper;
